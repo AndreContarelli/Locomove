b12da42b7e263188824d729e434cf8430474a409
André Contarelli Lima <a.contarellilima@gmail.com>
2026-06-22 15:50:51 -0300

    feat: architecture MVVM implemented with placeholders

---
+++ Checkin/Checkin/CheckinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckinApp.swift
+//  Checkin
+//
+//  Created by André Contarelli Lima on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CheckinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checkin/Checkin/ViewModel/ViewModelPlaceHolder.swift
@@ -0,0 +1,7 @@
+//
+//  ViewModelPlaceHolder.swift
+//  Checkin
+//
+//  Created by André Contarelli Lima on 22/06/26.
+//
+
